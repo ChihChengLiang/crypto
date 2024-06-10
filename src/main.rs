8c3e17d798107e4566900d42591ad9bba1d9c4e0
ChihChengLiang <chihchengliang@gmail.com>
2024-06-10 19:13:11 +0800

    impl matrix add

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    fmt::{write, Display},
+    fmt::Display,
     ops::{Add, Mul},
 };
 
@@ -49,6 +49,10 @@ impl PartialEq for PrimeField {
     }
 }
 
+fn fp11(n: u64) -> PrimeField {
+    PrimeField::new(n, 11)
+}
+
 #[derive(Debug)]
 struct Matrix<const NROW: usize, const NCOL: usize, ELEMENT> {
     elements: Vec<Vec<ELEMENT>>,
@@ -82,23 +86,57 @@ impl<const NROW: usize, const NCOL: usize, ELEMENT: Clone + Display> Display
     }
 }
 
+impl<const NROW: usize, const NCOL: usize, ELEMENT: Clone + Add<Output = ELEMENT>> Add
+    for Matrix<NROW, NCOL, ELEMENT>
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            elements: self
+                .elements
+                .iter()
+                .zip(rhs.elements.iter())
+                .map(|(self_row, rhs_row)| {
+                    let new_row: Vec<ELEMENT> = self_row
+                        .iter()
+                        .zip(rhs_row.iter())
+                        .map(|(self_elem, rhs_elem)| self_elem.clone() + rhs_elem.clone())
+                        .collect();
+                    new_row
+                })
+                .collect::<Vec<_>>(),
+        }
+    }
+}
+
+impl<const NROW: usize, const NCOL: usize, ELEMENT: PartialEq> PartialEq
+    for Matrix<NROW, NCOL, ELEMENT>
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.elements == other.elements
+    }
+}
+
 mod tests {
     use super::*;
 
     #[test]
     fn test_prime_field() {
-        let a = PrimeField::new(3, 11);
-        let b = PrimeField::new(5, 11);
-        assert_eq!(a.clone() + b.clone(), PrimeField::new(8, 11));
-        assert_eq!(a * b, PrimeField::new(4, 11));
+        let a = fp11(3);
+        let b = fp11(5);
+        assert_eq!(a.clone() + b.clone(), fp11(8));
+        assert_eq!(a * b, fp11(4));
     }
     #[test]
     fn test_matrix() {
-        let m: Matrix<2, 2, PrimeField> = Matrix::new(&[
-            &[PrimeField::new(3, 11), PrimeField::new(4, 11)],
-            &[PrimeField::new(5, 11), PrimeField::new(6, 11)],
-        ]);
-        println!("Matrix {m}")
+        let matrix_a: Matrix<2, 2, PrimeField> =
+            Matrix::new(&[&[fp11(3), fp11(4)], &[fp11(5), fp11(6)]]);
+        let matrix_b: Matrix<2, 2, PrimeField> =
+            Matrix::new(&[&[fp11(7), fp11(8)], &[fp11(9), fp11(10)]]);
+        let a_plus_b: Matrix<2, 2, PrimeField> =
+            Matrix::new(&[&[fp11(10), fp11(1)], &[fp11(3), fp11(5)]]);
+        assert_eq!(matrix_a + matrix_b, a_plus_b)
     }
 }
 
