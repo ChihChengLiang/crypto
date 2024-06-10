6964a83cd7ffe5635c22aed5b2e26152be15fee5
ChihChengLiang <chihchengliang@gmail.com>
2024-06-10 18:57:17 +0800

    impl matric

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::ops::{Add, Mul};
+use std::{
+    fmt::{write, Display},
+    ops::{Add, Mul},
+};
 
 #[derive(Debug, Clone)]
 struct PrimeField {
@@ -9,6 +12,11 @@ struct PrimeField {
 impl PrimeField {
     fn new(n: u64, p: u64) -> Self {
         Self { n: n % p, p }
+    }
+}
+impl Display for PrimeField {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Fp{}", self.n)
     }
 }
 
@@ -41,6 +49,39 @@ impl PartialEq for PrimeField {
     }
 }
 
+#[derive(Debug)]
+struct Matrix<const NROW: usize, const NCOL: usize, ELEMENT> {
+    elements: Vec<Vec<ELEMENT>>,
+}
+
+impl<const NROW: usize, const NCOL: usize, ELEMENT: Clone> Matrix<NROW, NCOL, ELEMENT> {
+    fn new(elements: &[&[ELEMENT]]) -> Self {
+        Self {
+            elements: elements
+                .iter()
+                .map(|v| v.to_vec())
+                .collect::<Vec<_>>()
+                .to_vec(),
+        }
+    }
+}
+impl<const NROW: usize, const NCOL: usize, ELEMENT: Clone + Display> Display
+    for Matrix<NROW, NCOL, ELEMENT>
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[\n")?;
+        for row in self.elements.iter() {
+            write!(f, "  [")?;
+            for elem in row.iter() {
+                write!(f, "{elem}, ")?;
+            }
+            write!(f, "]\n")?;
+        }
+
+        write!(f, "]\n")
+    }
+}
+
 mod tests {
     use super::*;
 
@@ -50,6 +91,14 @@ mod tests {
         let b = PrimeField::new(5, 11);
         assert_eq!(a.clone() + b.clone(), PrimeField::new(8, 11));
         assert_eq!(a * b, PrimeField::new(4, 11));
+    }
+    #[test]
+    fn test_matrix() {
+        let m: Matrix<2, 2, PrimeField> = Matrix::new(&[
+            &[PrimeField::new(3, 11), PrimeField::new(4, 11)],
+            &[PrimeField::new(5, 11), PrimeField::new(6, 11)],
+        ]);
+        println!("Matrix {m}")
     }
 }
 
